072b180673146649ae9eb2003d1c17c49699d0ed
Eric McCullough <eric96@live.missouristate.edu>
2020-09-14 16:55:26 -0500

    Add iptables to router

---
+++ router/install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get upgrade
+
+apt-get -y install --no-install-recommends iptables
+
+apt-get clean
+
+rm -rf /var/lib/apt/lists/*
+
