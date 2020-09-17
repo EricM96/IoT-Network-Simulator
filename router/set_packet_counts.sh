6801545805549caa11b4513fc0b32c7a43a67f1a
Eric McCullough <eric96@live.missouristate.edu>
2020-09-17 19:23:44 +0000

    End remote work

--- router/set_packet_counts.sh
+++ router/set_packet_counts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 
+iptables -t nat -A PREROUTING -p tcp --source echo-client --dport 8080 -j DNAT --to-destination echo-server:8080
+iptables -t nat -A PREROUTING -p tcp --source echo-server --dport 8080 -j DNAT --to-destination echo-cleint:8080
+
 iptables -N echo_client_tcp_in
 iptables -N echo_client_tcp_out
 iptables -A echo_client_tcp_in
