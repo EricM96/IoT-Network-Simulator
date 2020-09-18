3824d32ff40a33505556d41dc039d17a6682a28c
Eric McCullough <eric96@live.missouristate.edu>
2020-09-18 17:40:22 -0500

    Add data aggregation route

--- router/get_packet_counts.sh
+++ router/get_packet_counts.sh
@@ -4,8 +4,8 @@ client_packets_out=$(iptables -nvL echo_client_tcp | awk 'FNR == 4 {print $1; ex
 server_packets_in=$(iptables -nvL echo_server_tcp | awk 'FNR == 3 {print $1; exit}')
 server_packets_out=$(iptables -nvL echo_server_tcp | awk 'FNR == 4 {print $1; exit}')
 
-echo "From client: ($client_packets_in, $client_packets_out)"
-echo "From server: ($server_packets_in, $server_packets_out)"
-
 iptables -Z echo_client_tcp
 iptables -Z echo_server_tcp
+
+echo "$client_packets_in $client_packets_out $server_packets_in $server_packets_out"
+
--- router/src/main.rs
+++ router/src/main.rs
@@ -1,8 +1,50 @@
-use std::process::Command;
-use std::time::Duration;
-use std::thread::sleep;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-fn main() -> std::io::Result<()> {
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::json::Json;
+use serde::Serialize;
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(Serialize)]
+struct HostTraffic(u16, u16);
+
+#[derive(Serialize)]
+struct TrafficWindow {
+    echo_client: HostTraffic,
+    echo_server: HostTraffic,
+}
+
+#[get("/")]
+fn index() -> Json<TrafficWindow> {
+    let cmd_handle = Command::new("./get_packet_counts.sh")
+        .output()
+        .expect("Failed to get packet counts");
+
+    let output = cmd_handle.stdout;
+    let output: String = String::from_utf8_lossy(&output).to_string();
+    let mut parts = output
+        .split_whitespace()
+        .map(|output| output.parse::<u16>());
+
+    let window = TrafficWindow {
+        echo_client: HostTraffic(
+            parts.next().unwrap().unwrap(),
+            parts.next().unwrap().unwrap(),
+        ),
+        echo_server: HostTraffic(
+            parts.next().unwrap().unwrap(),
+            parts.next().unwrap().unwrap(),
+        ),
+    };
+
+    Json(window)
+}
+
+fn main() {
     let cmd_handle = Command::new("./set_packet_counts.sh")
         .output()
         .expect("Failed to run command");
@@ -10,9 +52,5 @@ fn main() -> std::io::Result<()> {
     let output = cmd_handle.stdout;
     println!("{}", String::from_utf8_lossy(&output));
 
-    // pause thread for one week
-    let pause = Duration::from_secs(7 * 24 * 60 * 60);
-    sleep(pause);
-
-    Ok(())
+    rocket::ignite().mount("/", routes![index]).launch();
 }
