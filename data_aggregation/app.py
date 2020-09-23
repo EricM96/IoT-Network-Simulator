d33b457211b4c2162a83aea209df859116c6e130
Eric McCullough <eric96@live.missouristate.edu>
2020-09-23 14:33:59 -0500

    Implement data aggregation module collection mode

--- data_aggregation/app.py
+++ data_aggregation/app.py
@@ -1,28 +1,48 @@
 from requests import get
 from time import sleep, time
 from pymongo import MongoClient
+from threading import Thread
 
 
-ROUTER = "http://router:8080/"
-INTERVAL = 15
+class DataAggregationModule(object):
+    router = "http://router:8080/"
+    interval = 15
 
-DB_USERNAME = "root"
-DB_PASSWORD = "password"
-DB_URL = f"mongodb://{DB_USERNAME}:{DB_PASSWORD}@db:27017/"
+    db_username = "root"
+    db_password = "password"
+    db_url = f"mongodb://{db_username}:{db_password}@db:27017/"
 
+    def __init__(self, label):
+        print('establishing mongo connection', flush=True)
+        db_client = MongoClient(self.db_url)
+        print('Connection established', flush=True)
+        db = db_client['traffic_windows']
+        self.col = db['test']
+        self.label = label
 
-def main():
-    db_client = MongoClient(DB_URL)
-    while True:
-        sleep(INTERVAL)
+    def _write_to_db(self, traffic_window):
+        print('starting db insertion', flush=True)
+        result = self.col.insert_one(traffic_window)
+        print(result, flush=True)
 
-        start = time()
-        r = get(ROUTER)
-        end = time()
-        print(type(r.json()), flush=True)
+    def main_loop(self):
+        while True:
+            sleep(self.interval)
 
-        print(f"Delay: {end - start:.2f}", flush=True)
+            start = time()
+            print('Request out', flush=True)
+            r = get(self.router)
+            end = time()
+            delay = end - start
+
+            traffic_window = r.json()
+            print('request received:', traffic_window)
+            traffic_window['delay'] = delay
+            traffic_window['label'] = self.label
+            print(traffic_window)
+            Thread(target=self._write_to_db, args=(traffic_window, )).run()
 
 
 if __name__ == "__main__":
-    main()
+    agg_module = DataAggregationModule('normal')
+    agg_module.main_loop()
