8f74d264b99eb54476937f128a2b8810f18999f4
Eric McCullough <eric96@live.missouristate.edu>
2020-09-28 05:47:36 +0000

    Add placeholder file for mongo folder

---
+++ tools/data_collection.py
@@ -0,0 +1,26 @@
+import subprocess
+import time
+
+
+def main():
+    scenarios = [
+                    './docker-compose/collect_ddos_3.yml',
+                    './docker-compose/collect_ddos_5.yml',
+                    './docker-compose/collect_norm_3.yml',
+                    './docker-compose/collect_norm_5.yml'
+                ]
+
+    for file in scenarios:
+        args = ['docker-compose', '-f', file, 'up', '-d']
+        p = subprocess.run(args, capture_output=True)
+        print(p.stdout)
+        print(p.stderr)
+        time.sleep(60)
+        args = ['docker-compose', '-f', file, 'stop']
+        p = subprocess.run(args, capture_output=True)
+        print(p.stdout)
+        print(p.stderr)
+
+
+if __name__ == '__main__':
+    main()
